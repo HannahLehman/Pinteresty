3f80d696a54e9349165cfcb32d95d73efc30415d
Hannah Lehman <hjlehman@gmail.com>
2014-12-13 15:02:49 -0500

    Add activeadmin gem and kaminari pagination.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://rubygems.org'
 
+gem 'activeadmin', github: 'activeadmin'
+
+gem 'kaminari'
+
 gem 'devise'
 
 gem 'carrierwave'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   delete '/pins' => 'pins#delete_all'
 
   get 'static_pages/home', as: :home
